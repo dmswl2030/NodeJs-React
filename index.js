be99a3bad3b1504ee5bf0f396719864f0f953b05
EUNJI <eunji9180@naver.com>
2025-02-02 22:09:38 +0900

    :sparkles: Env 설정 추가

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 const express = require('express');
 const app = express();
 const port = 5001;
-
+const config = require('./config/key');
 //몽고 DB 연결
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://devdmswl:L3PH8pnbmbZh3ReI@cluster0.silam.mongodb.net/'
-  )
+  .connect(config.mongoURI)
   .then(() => console.log('MogoDB 연결!'))
   .catch((error) => console.log('failed: ', error));
 
@@ -18,7 +16,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('시작 페이지'));
 //회원가입 시 필요한 정보들을 client에서 가져오면 그 정보를 db에 넣어준다 (콜백방식을 mongoose 7부터 지원하지 않음)
 app.post('/register', (req, res) => {
   const user = new User(req.body);
