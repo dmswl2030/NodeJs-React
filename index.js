f3ae4046a79416eafbd5b3c6e632c154fbd9ee53
EUNJI <eunji9180@naver.com>
2025-02-02 16:39:42 +0900

    :sparkles: Connect User & postman test

--- index.js
+++ index.js
@@ -12,5 +12,20 @@ mongoose
   .then(() => console.log('MogoDB 연결!'))
   .catch((error) => console.log('failed: ', error));
 
+//유저
+const { User } = require('./models/User');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('Hello World!'));
+//회원가입 시 필요한 정보들을 client에서 가져오면 그 정보를 db에 넣어준다 (콜백방식을 mongoose 7부터 지원하지 않음)
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then(() => res.status(200).json({ success: true }))
+    .catch((err) => res.status(400).json({ success: false, error: err }));
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
